4a03068300a728a5395b39228bc6016586514c63
Alan Peh <alanpeh87@gmail.com>
2024-10-09 20:39:02 +0800

    docker images upload

--- terraform/modules/ec2/vpc.tf
+++ terraform/modules/ec2/vpc.tf
@@ -4,12 +4,12 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   # Existing VPC ID
-#  name = "my-vpc" # change it to existing
-#  cidr = "10.0.0.0/16"
+  name = "my-vpc" # change it to existing
+  cidr = "10.0.0.0/16"
 
-#  azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
-#  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
   tags = {
     terraform = "true"
